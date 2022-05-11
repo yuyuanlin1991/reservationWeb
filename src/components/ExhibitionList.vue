6eb2477862d54726670c6d45a7665e9f55113da6
yuyuanlin1991 <yuyuanlin1991@163.com>
2022-05-11 20:05:24 +0800

    引入请求

---
+++ src/components/ExhibitionList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="room-list-container">
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ExhibitionList',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
